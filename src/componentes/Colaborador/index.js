5056966cd50fbb345404c6a1c21e5d148ec42cae
Bryan Assunção <bryan.assuncao@outlook.com.br>
2025-11-12 08:27:50 -0300

    adicionando mais componentes

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src="https://github.com/bryan-assuncao.png" alt="Rosto Pessoa" />
+            </div>
+            <div className='rodape'>
+                <h4>Bryan</h4>
+                <h5>Dev Back-end</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,59 @@
+import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
+import { useState } from "react";
 
-const Formulario = () => {
+const Formulario = (props) => {
+
+
+  const [nome, setNome] = useState('')
+  const [cargo, setCargo] = useState('')
+  const [imagem, setImagem] = useState('')
+  const [time, setTime] = useState('')
+
+  const aoSalvar = (evento) => {
+    evento.preventDefault()
+    props.aoColaboradorCadastrado({
+      nome,
+      cargo,
+      imagem,
+      time
+    })
+  }
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador.</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+          value={nome}
+          aoAlterar={value => setNome(value)}
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite seu cargo"
+          value={cargo}
+          aoAlterar={value => setCargo(value)}
+        />
+        <CampoTexto
+          label="Imagem"
+          placeholder="Informe o endereço da imagem"
+          value={imagem}
+          aoAlterar={value => setImagem(value)}
+        />
+        <ListaSuspensa
+          obrigatorio={true}
+          label="Time" itens={props.nomeDosTimes}
+          value={time}
+          aoAlterar={value => setTime(value)}
+        />
+        <Botao>Criar card</Botao>
       </form>
     </section>
   );
